e35296f8c04fc30910ff3ccc907b005fd5862446
Santo Shakil <shakil.mezbah@gmail.com>
2023-07-16 19:47:33 +0600

    Fixes

--- migrations/2023-07-16-080315_create_users/up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    age INTEGER NOT NULL
-);
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,57 @@
 pub mod schema;
 
 use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
 
-use crate::schema::users;
+use crate::schema::users as users_table;
+use crate::schema::users::dsl::*;
 
-#[derive(Selectable, Queryable, Insertable, Debug)]
-#[diesel(table_name = users)]
-struct NewUser<'a> {
-    name: &'a str,
-    age: i32,
+#[derive(Queryable, Selectable, Insertable, Debug)]
+#[diesel(table_name = crate::schema::users)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+struct User {
+    pub id: Option<i32>,
+    pub name: String,
+    pub age: i32,
+}
+
+impl User {
+    pub fn new(u_name: &str, u_age: i32) -> Self {
+        Self {
+            id: None,
+            name: u_name.to_string(),
+            age: u_age,
+        }
+    }
 }
 
 fn main() {
-    let mut connection =
-        SqliteConnection::establish("mydb.sqlite3").expect("Error connecting to database");
+    dotenv().ok();
 
-    diesel::insert_into(users::table)
-        .values(&vec![
-            NewUser {
-                name: "Alice",
-                age: 42,
-            },
-            NewUser {
-                name: "Bob",
-                age: 69,
-            },
-        ])
-        .execute(&mut connection)
-        .expect("Error inserting users");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let connection = &mut SqliteConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url));
 
-    let all_users = users::table
-        .load::<NewUser>(&mut connection)
+    let new_user = User::new("John", 20);
+
+    diesel::insert_into(users_table::table)
+        .values(&new_user)
+        .execute(connection)
+        .expect("Error inserting new user");
+
+    let all_users: Vec<User> = users
+        .filter(age.gt(20))
+        .limit(5)
+        .load(connection)
         .expect("Error loading users");
+
+    for user in all_users {
+        println!(
+            "id: {}, name: {}, age: {}",
+            user.id.unwrap(),
+            user.name,
+            user.age
+        );
+    }
 }
